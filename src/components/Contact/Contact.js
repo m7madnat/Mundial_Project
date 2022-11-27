268d7f1e0e1c01fbfd4e4ac055940f4116ce6942
m7madnat <m7madnatseh1999@gmail.com>
2022-11-27 12:53:36 +0200

    fixes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,12 @@
 const Contact = () => {
   return (
     <div>
-      <form name="contact" netlify netlify-honeypot="bot-field">
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message"></textarea>
+      <form name="contact" action="/success" method="POST">
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="text" id="name" name="name" placeholder="Name" required />
+        <input type="email" id="email" name="email" placeholder="Email" required />
+        <textarea id="message" name="message" placeholder="Message" required></textarea>
+        <button type="submit">Send</button>
       </form>
     </div>
   );
